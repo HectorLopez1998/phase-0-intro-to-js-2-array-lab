b883cb9cb516a40070399378085f231ab42f0fbf
hector <hectorlopezp1998@gmail.com>
2022-05-02 09:43:09 -0400

    commit tree 4d9c116874411d1d882f1371d841ac37c2723c02

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-// Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"]
-
-function destructivelyAppendCat(name){
-    return cats.push(name)
-}
-
-function destructivelyPrependCat(name){
-    return cats.unshift(name)
-}
-
-function destructivelyRemoveLastCat(){
-    return cats.pop()
-}
-
-function destructivelyRemoveFirstCat(){
-    return cats.shift()
-}
-
-function appendCat(name){
-    return [...cats, name]
-}
-
-function prependCat(name){
-    return [name, ...cats]
-}
-
-
-function removeLastCat(){
-    return cats.slice(0, cats.length -1)
-}
-
-function removeFirstCat(){
-    return cats.slice(1)
-}
+// Write your solution here
+const cats = ['Milo', 'Otis', 'Garfield']
+const destructivelyAppendCat = (name) => { cats.push(name) }
+const destructivelyPrependCat = (name) => { cats.unshift(name) }
+const destructivelyRemoveLastCat = (name) => { cats.pop(name) }
+const destructivelyRemoveFirstCat = (name) => { cats.shift(name) }
+const appendCat = (name) => { return [...cats, name] }
+const prependCat = (name) => { return [name, ...cats] }
+const removeLastCat = (name) => { return cats.slice(0, -1) }
+const removeFirstCat = (name) => { return cats.slice(1) }
